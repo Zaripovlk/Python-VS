22bbb2e99e7cec3983d76e502011276c9f562d84
Zaripovlk <zaripovlk@gmail.com>
2024-04-02 19:52:38 +0300

    add HW in Lesson 4

--- Lesson 4/HW1.py
+++ Lesson 4/HW1.py
@@ -8,17 +8,38 @@
 
 # На входе:
 
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 
-# var1 = '5 4' # количество элементов первого и второго множества
-# var2 = '1 3 5 7 9' # элементы первого множества через пробел
-# var3 = '2 3 4 5' # элементы второго множества через пробел
+# var1 = "5 4" # количество элементов первого и второго множества
+# var2 = "1 3 5 7 9" # элементы первого множества через пробел
+# var3 = "2 3 4 5" # элементы второго множества через пробел
 # На выходе:
 
 
-# 3 5
-import random
-var1= input( "введите количество элементов первого множества: ")
-# per2=int (input ("введите количество элементов второго множества: "))
-print (var1)
-var2= {random.randint(0, 10) for i in range(var1(0,0))}
-print ({var2})
+
+# var1  = [int (num) for num in var1.split()]
+# var2  = [int (num) for num in var2.split()]
+# var3  = [int (num) for num in var3.split()]
+# len1, len2 = var1
+# result=[]
+# for i in range (len1):
+#     for j in range (len2):
+#         if var2[i] == var3[j]:
+#             result.append(var3[j])
+# # result.sort() первый вариант сортировки
+# # print (sorted(result)) второй вариант сортировки
+# for i in range(len(result)-1):
+#     if result[i] > result[i+1]:
+#         result[i], result[i+1] = result[i+1], result [i]
+# print (result) #первый вариант решения
+var1 = '5 5'
+var2 = '10 20 30 40 50'
+var3 = '10 20 30 40 50'
+var2  = [int (num) for num in var2.split()]
+var3  = [int (num) for num in var3.split()]
+var2 = set(var2)
+print (var2)
+var3 = set(var3)
+result = var2 & var3 
+print(*sorted(result))
